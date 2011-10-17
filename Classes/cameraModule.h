4a33887d9addcf45ecccf7f3335327f57fa432d5
Cornell IDL <HCL@rrdhcp232-206.redrover.cornell.edu>
2011-10-16 23:44:57 -0400

    First Commit

---
+++ Classes/cameraModule.h
@@ -0,0 +1,16 @@
+//
+//  cameraModule.h
+//  audience
+//
+//  Created by Cornell IDL on 9/28/11.
+//  Copyright 2011 High Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface cameraModule : UIViewController {
+    
+}
+
+@end
---
+++ Classes/voiceRecordModule.h
@@ -0,0 +1,16 @@
+//
+//  voiceRecordModule.h
+//  audience
+//
+//  Created by Cornell IDL on 9/28/11.
+//  Copyright 2011 High Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface voiceRecordModule : UIViewController {
+    
+}
+
+@end
